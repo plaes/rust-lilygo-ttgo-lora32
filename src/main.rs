f9ecb62a5df5aa115fedf2510b6210dde971e158
Priit Laes <plaes@plaes.org>
2023-05-27 21:34:24 +0300

    feat: Add ssd1306/embedded-graphics

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,23 @@
 
 use esp_backtrace as _;
 use esp_println::println;
-use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc};
+use hal::{
+    clock::ClockControl, gpio::IO, i2c::I2C, peripherals::Peripherals, prelude::*,
+    timer::TimerGroup, Rtc,
+};
+
+use embedded_graphics::{
+    mono_font::{
+        ascii::{FONT_6X10, FONT_9X18_BOLD},
+        MonoTextStyleBuilder,
+    },
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Alignment, Text},
+};
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
+
+use nb::block;
 
 #[entry]
 fn main() -> ! {
@@ -11,27 +27,90 @@ fn main() -> ! {
     let mut system = peripherals.DPORT.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    // Disable the RTC and TIMG watchdog timers
-    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    rtc.rwdt.disable();
-
     let timer_group0 = TimerGroup::new(
         peripherals.TIMG0,
         &clocks,
         &mut system.peripheral_clock_control,
     );
-    let mut wdt0 = timer_group0.wdt;
-    wdt0.disable();
+    let mut timer0 = timer_group0.timer0;
+    let mut wdt = timer_group0.wdt;
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
 
-    let timer_group1 = TimerGroup::new(
-        peripherals.TIMG1,
-        &clocks,
+    // Disable watchdog timer
+    wdt.disable();
+    rtc.rwdt.disable();
+
+    // Initialize IO
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let i2c = I2C::new(
+        peripherals.I2C0,
+        io.pins.gpio4,
+        io.pins.gpio15,
+        100u32.kHz(),
         &mut system.peripheral_clock_control,
+        &clocks,
     );
-    let mut wdt1 = timer_group1.wdt;
-    wdt1.disable();
 
-    println!("Hello world!");
+    timer0.start(5u64.secs());
 
-    loop {}
+    let iface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(iface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+    let text_style_big = MonoTextStyleBuilder::new()
+        .font(&FONT_9X18_BOLD)
+        .text_color(BinaryColor::On)
+        .build();
+
+    println!("Starting main loop!");
+
+    loop {
+        Text::with_alignment(
+            "esp-hal",
+            display.bounding_box().center() + Point::new(0, 0),
+            text_style_big,
+            Alignment::Center,
+        )
+        .draw(&mut display)
+        .unwrap();
+
+        Text::with_alignment(
+            "Chip: ESP32",
+            display.bounding_box().center() + Point::new(0, 14),
+            text_style,
+            Alignment::Center,
+        )
+        .draw(&mut display)
+        .unwrap();
+
+        // Write buffer to display
+        display.flush().unwrap();
+        // Clear display buffer
+        display.clear();
+
+        // Wait 5 seconds
+        block!(timer0.wait()).unwrap();
+
+        Text::with_alignment(
+            "Demo!",
+            display.bounding_box().center(),
+            text_style_big,
+            Alignment::Center,
+        )
+        .draw(&mut display)
+        .unwrap();
+
+        // Write buffer to display
+        display.flush().unwrap();
+        // Clear display buffer
+        display.clear();
+
+        // Wait 5 seconds
+        block!(timer0.wait()).unwrap();
+    }
 }
