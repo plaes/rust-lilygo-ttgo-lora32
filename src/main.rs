89c2f280ce3f7f6020301551b912fa6a510f4ffa
Priit Laes <plaes@plaes.org>
2023-06-05 21:46:47 +0300

    Migrate to embassy

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(type_alias_impl_trait)]
 
-use esp_backtrace as _;
 use esp_println::println;
-use hal::{
-    clock::ClockControl, gpio::IO, i2c::I2C, peripherals::Peripherals, prelude::*,
-    timer::TimerGroup, Rtc,
-};
-
 use embedded_graphics::{
     mono_font::{
         ascii::{FONT_6X10, FONT_9X18_BOLD},
@@ -19,41 +14,26 @@ use embedded_graphics::{
 };
 use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 
-use nb::block;
+use embassy_executor::Executor;
+use embassy_time::{Duration, Timer};
+use hal::{
+    clock::ClockControl,
+    embassy,
+    gpio::IO,
+    i2c::I2C,
+    peripherals::{I2C0, Peripherals},
+    prelude::*,
+    timer::TimerGroup,
+    Rtc,
+};
+use esp_backtrace as _;
+use static_cell::StaticCell;
 
-#[entry]
-fn main() -> ! {
-    let peripherals = Peripherals::take();
-    let mut system = peripherals.DPORT.split();
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
-
-    let timer_group0 = TimerGroup::new(
-        peripherals.TIMG0,
-        &clocks,
-        &mut system.peripheral_clock_control,
-    );
-    let mut timer0 = timer_group0.timer0;
-    let mut wdt = timer_group0.wdt;
-    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-
-    // Disable watchdog timer
-    wdt.disable();
-    rtc.rwdt.disable();
-
-    // Initialize IO
-    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
-    let i2c = I2C::new(
-        peripherals.I2C0,
-        io.pins.gpio4,
-        io.pins.gpio15,
-        100u32.kHz(),
-        &mut system.peripheral_clock_control,
-        &clocks,
-    );
-
-    timer0.start(5u64.secs());
-
+#[embassy_executor::task]
+async fn handle_display(i2c: I2C<'static, I2C0>) {
     let iface = I2CDisplayInterface::new(i2c);
+    println!("Starting display loop!");
+
     let mut display = Ssd1306::new(iface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
     display.init().unwrap();
@@ -67,9 +47,9 @@ fn main() -> ! {
         .text_color(BinaryColor::On)
         .build();
 
-    println!("Starting main loop!");
-
     loop {
+        esp_println::println!("task: display!");
+
         Text::with_alignment(
             "esp-hal",
             display.bounding_box().center() + Point::new(0, 0),
@@ -93,8 +73,7 @@ fn main() -> ! {
         // Clear display buffer
         display.clear();
 
-        // Wait 5 seconds
-        block!(timer0.wait()).unwrap();
+        Timer::after(Duration::from_millis(3_000)).await;
 
         Text::with_alignment(
             "Demo!",
@@ -110,7 +89,62 @@ fn main() -> ! {
         // Clear display buffer
         display.clear();
 
-        // Wait 5 seconds
-        block!(timer0.wait()).unwrap();
+        Timer::after(Duration::from_millis(3_000)).await;
     }
+}
+
+#[embassy_executor::task]
+async fn run2() {
+    loop {
+        esp_println::println!("task: 2!");
+        Timer::after(Duration::from_millis(5_000)).await;
+    }
+}
+
+static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+
+#[entry]
+fn main() -> ! {
+    esp_println::println!("Init!");
+    let peripherals = Peripherals::take();
+    let mut system = peripherals.DPORT.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
+    let timer_group0 = TimerGroup::new(
+        peripherals.TIMG0,
+        &clocks,
+        &mut system.peripheral_clock_control,
+    );
+    let mut wdt0 = timer_group0.wdt;
+    let timer_group1 = TimerGroup::new(
+        peripherals.TIMG1,
+        &clocks,
+        &mut system.peripheral_clock_control,
+    );
+    let mut wdt1 = timer_group1.wdt;
+
+    // Disable watchdog timers
+    rtc.rwdt.disable();
+    wdt0.disable();
+    wdt1.disable();
+
+    embassy::init(&clocks, timer_group0.timer0);
+
+    // Initialize IO
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let i2c = I2C::new(
+        peripherals.I2C0,
+        io.pins.gpio4,
+        io.pins.gpio15,
+        100u32.kHz(),
+        &mut system.peripheral_clock_control,
+        &clocks,
+    );
+
+    let executor = EXECUTOR.init(Executor::new());
+    executor.run(|spawner| {
+        spawner.spawn(handle_display(i2c)).ok();
+        spawner.spawn(run2()).ok();
+    });
 }
