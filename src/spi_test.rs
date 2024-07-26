9ddd03b94c287baf7662dd80eee2d12811b3d8bf
Priit Laes <plaes@plaes.org>
2024-07-26 12:33:53 +0300

    spi_test: Add 3rd testcase with odd behaviour

--- src/spi_test.rs
+++ src/spi_test.rs
@@ -93,23 +93,30 @@ async fn main(_spawner: Spawner) {
     let write = [0xde, 0xad, 0xbe, 0xef];
     let mut read: [u8; 4] = [0x00u8; 4];
 
-    defmt::info!("TX...");
+    defmt::info!("SPI 1...");
     // TODO: defmt::unwrap!(...)
     let _ = spi_dev.transfer(&mut read[..], &write[..]).await;
 
     defmt::info!("...done");
     assert_eq!(write, read);
 
-    let write_buf = &[0xaa];
+    let write_buf = &[1];
     //let mut payload = &[0xd1, 0xd2, 0xd3, 0xd4];
     // Works with 1..4 bytes
     // Hangs when payload is > 4 bytes??
-    let payload = &[0xd1, 0xd2, 0xd3, 0xd5, 0xd5];
+    let payload = &[5, 4, 3, 2, 1];
 
-    defmt::info!("TX... ");
+    defmt::info!("SPI 2 (write buffer)... ");
     let mut ops = [Operation::Write(write_buf), Operation::Write(payload)];
     let _ = spi_dev.transaction(&mut ops).await;
     defmt::info!("...done");
+
+    defmt::info!("SPI 3 (read previous buffer)...");
+    let mut read: [u8; 6] = [0x00u8; 6];
+    let mut ops = [Operation::Read(&mut read)];
+    let _ = spi_dev.transaction(&mut ops).await;
+    defmt::info!("... result: {:?}", read);
+    // TODO: Assert that write_buf + payload == read ?
 
     loop {
         defmt::info!("MAIN LOOP!");
